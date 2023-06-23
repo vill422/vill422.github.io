c8548b63ca2fba10b037822ab958bb4718a8a6cd
Wei Guo <qiangzheguo@gmail.com>
2023-06-23 15:23:24 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,25 @@
 /**
  * @type {import('next').NextConfig}
  */
+// const nextConfig = {
+//   images: {
+//     loader: "akamai",
+//     path: "",
+//   },
+//   assetPrefix: "./",
+// };
+
+// export default nextConfig;
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  assetPrefix: "./",
+  output: "export",
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
